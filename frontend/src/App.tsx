efeeba0a50ad15055ad83f3288a27b4bd46dfcab
Lalit Wagh <lalitwagh2804@gmail.com>
2026-05-02 16:45:10 +0530

    Auth design changed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes ,Route } from "react-router";
 import Auth from "./Pages/Auth";
 import Dashboard from "./Pages/Dashboard"
-
+import Terms from "./Pages/Terms";
+import Privacy from "./Pages/Privacy";
 export function App() {
   return (
    <BrowserRouter>
     <Routes>
       <Route path="/auth" element={<Auth/>}/>
       <Route path="/" element={<Dashboard/>}/>
+      <Route path="/terms" element={<Terms />} />
+<Route path="/privacy" element={<Privacy />} />
     </Routes>
    </BrowserRouter>
   );
